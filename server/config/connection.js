8309b9ea64f7de9a72b42bee355df8ba494e49ab
fycn <fannychen623@gmail.com>
2024-03-04 20:50:54 -0500

    connected to MongoDB

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,19 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({path:'server/.env'});
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/dietPlanner', {
+mongoose.set('strictQuery', true);
+
+mongoose.connect(process.env.MONGODB_CONNECT_URI || 'mongodb://127.0.0.1:27017/dietPlanner', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-});
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  }
+);
 
 module.exports = mongoose.connection;
